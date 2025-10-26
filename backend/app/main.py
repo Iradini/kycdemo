b7b97e02076b72ca8f0207477b54c8ca794eb77a
Iradini <mariairadini@gmail.com>
2025-10-25 21:04:47 -0300

    Created FE configs

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
-from app.api.v1.routed import router as api_router
+from app.api.v1.routes import router as api_router
 
-app = FastAPI(title="KYC Demo Application", version='1.0.0')
+app = FastAPI(title="KYC Demo Application", version='0.1.0')
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[settings.cors_origins],
